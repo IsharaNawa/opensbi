717cfa6041108cb1dd167e39bb93cd54421d5fb5
IsharaNawa <ikos0266@uni.sydney.edu.au>
2025-11-24 02:08:20 +1100

    fix: update genesys2_sbi_dtb.sh to use absolute paths for OpenSBI and U-Boot binaries

--- genesys2_sbi_dtb.sh
+++ genesys2_sbi_dtb.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/bash
 set -e
-OPENSBI_DIR=~/opensbi
+OPENSBI_DIR=~/Research/repos/opensbi
 cd $OPENSBI_DIR
 make distclean
 make PLATFORM=generic \
     CROSS_COMPILE=riscv64-linux-gnu- \
-    FW_PAYLOAD_PATH=~/u-boot/u-boot-dtb.bin \
+    FW_PAYLOAD_PATH=~/Research/repos/u-boot/u-boot-dtb.bin \
     -j16
 echo "OpenSBI build completed."
 strings build/platform/generic/firmware/fw_payload.elf | grep chipyard
 
-cp ~/opensbi/build/platform/generic/firmware/fw_payload.bin ~/u-boot/u-boot-sbi.bin
-riscv64-linux-gnu-objdump -h  ~/opensbi/build/platform/generic/firmware/fw_payload.elf | grep dtb
+cp ~/Research/repos/opensbi/build/platform/generic/firmware/fw_payload.bin ~/Research/repos/u-boot/u-boot-sbi.bin
+riscv64-linux-gnu-objdump -h  ~/Research/repos/opensbi/build/platform/generic/firmware/fw_payload.elf | grep dtb
 echo "You can now use this binary with your RISC-V platform."
 cd -
 echo "Returning to the previous directory."
 echo "Script execution finished."
+
+# this fails because u-boot-dtb.bin might not exist yet
