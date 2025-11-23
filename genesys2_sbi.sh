ab91c6c09cef0ef8a0cb9fd4041426ec8b39c976
IsharaNawa <ikos0266@uni.sydney.edu.au>
2025-11-24 01:48:53 +1100

    fix: update paths in genesys2_sbi.sh to use absolute references

--- genesys2_sbi.sh
+++ genesys2_sbi.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/bash
 set -e
-OPENSBI_DIR=~/opensbi
+OPENSBI_DIR=~/Research/repos/opensbi
 cd $OPENSBI_DIR
 make distclean
 make PLATFORM=generic FW_DEBUG=1 \
     CROSS_COMPILE=riscv64-linux-gnu- \
-    FW_PAYLOAD_PATH=~/u-boot/u-boot.bin \
-    FW_FDT_PATH=~/u-boot/arch/riscv/dts/chipyard.fpga.genesys2.GENESYS2FPGATestHarness.RocketGENESYS2Config.dtb \
+    FW_PAYLOAD_PATH=~/Research/repos/u-boot/u-boot.bin \
+    FW_FDT_PATH=~/Research/repos/u-boot/arch/riscv/dts/chipyard.fpga.genesys2.GENESYS2FPGATestHarness.RocketGENESYS2Config.dtb \
     -j16
 echo "OpenSBI build completed."
 strings build/platform/generic/firmware/fw_payload.elf | grep chipyard
 
-cp ~/opensbi/build/platform/generic/firmware/fw_payload.bin ~/u-boot/u-boot-sbi.bin
-riscv64-linux-gnu-objdump -h  ~/opensbi/build/platform/generic/firmware/fw_payload.elf | grep dtb
+cp ~/Research/repos/opensbi/build/platform/generic/firmware/fw_payload.bin ~/Research/repos/u-boot/u-boot-sbi.bin
+riscv64-linux-gnu-objdump -h  ~/Research/repos/opensbi/build/platform/generic/firmware/fw_payload.elf | grep dtb
 echo "You can now use this binary with your RISC-V platform."
 cd -
 echo "Returning to the previous directory."
